7bcda45f9bd30cec9f57f28bdce2d608f794e1f7
Rafael Jesus do Prado <rafaeljprado@gmail.com>
2022-04-26 22:30:59 -0300

    add global style

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <title>Início | ig.news</title>
+      <h1 className="stles.title">Ola</h1>
+    </>
+  );
+}
